ba27e61dd0c5033fb0592f28951f888e48a08ea4
faahta <fasso143@gmail.com>
2018-09-30 17:34:36 +0200

    primeng test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TeachingPorta-Web';
+  items: MenuItem[];
+  activeItem: MenuItem;
+
+  ngOnInit() {
+    this.items = [
+      {label: 'Stats', icon: 'fa fa-fw fa-bar-chart'},
+      {label: 'Calendar', icon: 'fa fa-fw fa-calendar'},
+      {label: 'Documentation', icon: 'fa fa-fw fa-book'},
+      {label: 'Support', icon: 'fa fa-fw fa-support'},
+      {label: 'Social', icon: 'fa fa-fw fa-twitter'}
+    ];
+  }
+
+  this.activeItem = this.items[2];
 }
